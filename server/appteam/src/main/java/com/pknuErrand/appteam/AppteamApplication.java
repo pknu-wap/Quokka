1cab44c4a9a8902523509756298c2d1f16397c9e
Lee Jehui <ls09094@gmail.com>
2024-03-28 22:44:53 +0900

    Init: Spring Boot 프로젝트 생성
    Spring Boot 프로젝트 생성
    - Spring 3.2.4
    - JDK 17

---
+++ server/appteam/src/main/java/com/pknuErrand/appteam/AppteamApplication.java
@@ -0,0 +1,13 @@
+package com.pknuErrand.appteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppteamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppteamApplication.class, args);
+	}
+
+}
---
+++ server/appteam/src/test/java/com/pknuErrand/appteam/AppteamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pknuErrand.appteam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppteamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
