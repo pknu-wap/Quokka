c2311467e48b4f08bac56313527c99151df0385c
unh6unh6 <ruddbs3308@naver.com>
2024-04-06 18:49:28 +0900

    Feat: errnad 전부 불러오기 dto 변
    buildMemberErrandDto 추가

--- server/appteam/src/main/java/com/pknuErrand/appteam/domain/errand/getDto/ErrandListResponseDto.java
+++ server/appteam/src/main/java/com/pknuErrand/appteam/domain/errand/getDto/ErrandListResponseDto.java
@@ -3,6 +3,7 @@ package com.pknuErrand.appteam.domain.errand.defaultDto;
 import com.pknuErrand.appteam.domain.errand.Errand;
 import com.pknuErrand.appteam.domain.errand.Status;
 import com.pknuErrand.appteam.domain.member.Member;
+import com.pknuErrand.appteam.domain.member.MemberErrandDto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,7 +14,7 @@ import java.sql.Timestamp;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ErrandListResponseDto { // from Entity
-    private Member orderNo; // 심부름 시킨사람
+    private MemberErrandDto order; // 심부름 시킨사람
 
     private Timestamp createdDate; // 등록한 date
 
@@ -24,20 +25,5 @@ public class ErrandListResponseDto { // from Entity
     private int reward; // 보수금액
 
     private Status status; // 상태
-
-    public ErrandResponseDto(Errand errand) {
-        this.orderNo = errand.getOrderNo();
-        this.createdDate = errand.getCreatedDate();
-        this.title = errand.getTitle();
-        this.destination = errand.getDestination();
-        this.latitude = errand.getLatitude();
-        this.longitude = errand.getLongitude();
-        this.due = errand.getDue();
-        this.detail = errand.getDetail();
-        this.reward = errand.getReward();
-        this.isCash = errand.isCash();
-        this.status = errand.getStatus();
-        this.erranderNo = errand.getErranderNo();
-    }
 
 }
--- server/appteam/src/main/java/com/pknuErrand/appteam/service/errand/ErrandService.java
+++ server/appteam/src/main/java/com/pknuErrand/appteam/service/errand/ErrandService.java
@@ -2,9 +2,12 @@ package com.pknuErrand.appteam.service.errand;
 
 import com.pknuErrand.appteam.domain.errand.Errand;
 import com.pknuErrand.appteam.domain.errand.ErrandBuilder;
+import com.pknuErrand.appteam.domain.errand.defaultDto.ErrandListResponseDto;
 import com.pknuErrand.appteam.domain.errand.defaultDto.ErrandRequestDto;
 import com.pknuErrand.appteam.domain.errand.defaultDto.ErrandResponseDto;
 import com.pknuErrand.appteam.domain.errand.saveDto.ErrandSaveRequestDto;
+import com.pknuErrand.appteam.domain.member.Member;
+import com.pknuErrand.appteam.domain.member.MemberErrandDto;
 import com.pknuErrand.appteam.repository.errand.ErrandRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -26,11 +29,12 @@ public class ErrandService {
     @Transactional
     public ErrandResponseDto createErrand(ErrandSaveRequestDto errandSaveRequestDto) {
         /**
-         *    Member orderMember = memberService.findMemberById(errandSaveRequestDto.getOrderNo());
          *    member service 추가되면 수정 필요
          */
+        Member orderMember = memberService.findMemberById(errandSaveRequestDto.getOrderNo());
+        
         Errand saveErrand = new ErrandBuilder()
-                .orderNo(errandSaveRequestDto.getOrderNo())
+                .orderNo(orderMember)
                 .createdDate(errandSaveRequestDto.getCreatedDate())
                 .title(errandSaveRequestDto.getTitle())
                 .destination(errandSaveRequestDto.getDestination())
@@ -48,12 +52,28 @@ public class ErrandService {
     }
 
     @Transactional
-    public List<ErrandResponseDto> findAllErrand() {
+    public List<ErrandListResponseDto> findAllErrand() {
         List<Errand> errandList = errandRepository.findAll();
-        List<ErrandResponseDto> errandResponseDtoList = new ArrayList<>();
-        for(Errand errand : errandList)
-            errandResponseDtoList.add(new ErrandResponseDto(errand));
-        return errandResponseDtoList;
+        List<ErrandListResponseDto> errandListResponseDtoList = new ArrayList<>();
+        for(Errand errand : errandList) {
+            MemberErrandDto memberErrandDto = buildMemberErrandDto(errand.getErranderNo());
+            ErrandListResponseDto errandListResponseDto = new ErrandListResponseDto(
+                    memberErrandDto, errand.getCreatedDate(), errand.getTitle(),
+                    errand.getDestination(), errand.getReward(), errand.getStatus()
+            );
+            errandListResponseDtoList.add(ErrandListResponseDto);
+        }
+        return errandListResponseDtoList;
+    }
+    @Transactional
+    /**
+     *    member domain 추가되면 확인 필요
+     */
+    public MemberErrandDto buildMemberErrandDto(Member member) {
+        long memberNo = member.getMemberNo();
+        String nickname = member.getNickname();
+        double score = member.getScore();
+        return new MemberErrandDto(memberNo, nickname, score);
     }
 
     @Transactional
