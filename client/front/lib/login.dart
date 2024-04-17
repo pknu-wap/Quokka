512afdd086c2fee6b442a65fd7e9dd23192300db
김수현 <suhyun_113@naver.com>
2024-04-17 22:27:45 +0900

    Feat: 로그인 페이지 분리 및 main.dart 새로운 메인 페이지 추가

--- client/front/lib/login.dart
+++ client/front/lib/login.dart
@@ -5,7 +5,6 @@ import 'sign_up.dart'; // 파일 호출
 
 void main() => runApp(MyApp());
 
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -16,7 +15,6 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
 
 class LogIn extends StatefulWidget {
   @override
@@ -45,17 +43,19 @@ class _LogInState extends State<LogIn> {
                   ),
                 ),
               ),
-              Container( // 길이 제한 필요
-                margin: EdgeInsets.only(left: 20.0, right: 20.0, top: 43.0), // 학번 텍스트 필드에 대한 마진 설정
+              Container(
+                // 길이 제한 필요
+                margin: EdgeInsets.only(left: 20.0, right: 20.0, top: 43.0),
+                // 학번 텍스트 필드에 대한 마진 설정
                 width: 320,
                 height: 50,
                 child: TextField(
-                maxLength: maxStudentIdLength, // 최대 길이 설정
-                onChanged: (text) {
-                  if (text.length > maxStudentIdLength){
-                    print('최대 $maxStudentIdLength자만 입력할 수 있습니다.');
-                }
-                },
+                  maxLength: maxStudentIdLength, // 최대 길이 설정
+                  onChanged: (text) {
+                    if (text.length > maxStudentIdLength) {
+                      print('최대 $maxStudentIdLength자만 입력할 수 있습니다.');
+                    }
+                  },
                   decoration: InputDecoration(
                     hintText: '학번',
                     hintStyle: TextStyle(
@@ -63,7 +63,8 @@ class _LogInState extends State<LogIn> {
                         color: Color(0xFF404040),
                         fontFamily: 'Pretendard',
                         fontWeight: FontWeight.w400),
-                    contentPadding: EdgeInsets.only(left: 17, right: 17), // 텍스트를 수직으로 가운데 정렬
+                    contentPadding: EdgeInsets.only(left: 17, right: 17),
+                    // 텍스트를 수직으로 가운데 정렬
                     filled: true,
                     fillColor: Color(0xFFE5E5E5),
                     labelStyle: TextStyle(color: Color(0xFFE5E5E5)),
@@ -83,13 +84,15 @@ class _LogInState extends State<LogIn> {
               ),
 
               Container(
-                margin: EdgeInsets.only(left: 20.0, right: 20.0, top: 6.0), // 비밀번호 텍스트 필드에 대한 마진 설정
+                margin: EdgeInsets.only(left: 20.0, right: 20.0, top: 6.0),
+                // 비밀번호 텍스트 필드에 대한 마진 설정
                 width: 320,
                 height: 50,
                 child: TextField(
-                  maxLength: maxPasswordLength, // 비밀번호 텍스트 필드의 최대 길이 설정
+                  maxLength: maxPasswordLength,
+                  // 비밀번호 텍스트 필드의 최대 길이 설정
                   onChanged: (text) {
-                    if (text.length < minPasswordLength){
+                    if (text.length < minPasswordLength) {
                       print('최소 $minPasswordLength자 이상 입력해주세요.');
                     } else if (text.length > maxPasswordLength) {
                       print('최대 $maxPasswordLength자만 입력할 수 있습니다.');
@@ -97,12 +100,13 @@ class _LogInState extends State<LogIn> {
                   },
                   decoration: InputDecoration(
                     hintText: '비밀번호',
-                    hintStyle: TextStyle(fontSize: 15,
+                    hintStyle: TextStyle(
+                        fontSize: 15,
                         color: Color(0xFF404040),
                         fontFamily: 'Pretendard',
-                        fontWeight: FontWeight.w400
-                    ),
-                    contentPadding: EdgeInsets.only(left: 17, right: 17), // 텍스트를 수직으로 가운데 정렬
+                        fontWeight: FontWeight.w400),
+                    contentPadding: EdgeInsets.only(left: 17, right: 17),
+                    // 텍스트를 수직으로 가운데 정렬
                     filled: true,
                     fillColor: Color(0xFFE5E5E5),
                     labelStyle: TextStyle(color: Colors.black),
@@ -132,13 +136,15 @@ class _LogInState extends State<LogIn> {
                   },
                   style: ButtonStyle(
                     // 버튼의 배경색 변경하기
-                    backgroundColor: MaterialStateProperty.all<Color>(Color(0xFF7C3D1A)), // 0xFF로 시작하는 16진수 색상 코드 사용,
+                    backgroundColor: MaterialStateProperty.all<Color>(
+                        Color(0xFF7C3D1A)), // 0xFF로 시작하는 16진수 색상 코드 사용,
                     // 버튼의 크기 정하기
                     minimumSize: MaterialStateProperty.all<Size>(Size(319, 50)),
                     // 버튼의 모양 변경하기
                     shape: MaterialStateProperty.all<RoundedRectangleBorder>(
                       RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10), // 원하는 모양에 따라 BorderRadius 조절
+                        borderRadius: BorderRadius.circular(
+                            10), // 원하는 모양에 따라 BorderRadius 조절
                       ),
                     ),
                   ),
@@ -177,7 +183,6 @@ class _LogInState extends State<LogIn> {
               ),
               // 비밀번호 찾기 버튼 구현(누르면 찾기 화면으로 이동)
 
-
               // GestureDetector(
               //   onTap: () {
               //     // 클릭 시 수행할 작업을 여기에 추가하세요
@@ -208,19 +213,19 @@ class _LogInState extends State<LogIn> {
                     // Respond to button press
                     // 누르면 다음 페이지로 이동
                     print('Login Button Clicked!');
-                    Navigator.of(context).push(
-                        MaterialPageRoute(builder: (context) => SignUpScreen())
-                    );
+                    Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => SignUpScreen()));
                   },
                   style: ButtonStyle(
-                    minimumSize: MaterialStateProperty.all(Size(100, 29)), // 버튼 크기
+                    minimumSize:
+                        MaterialStateProperty.all(Size(100, 29)), // 버튼 크기
                   ),
                   child: Text(
                     "회원가입",
                     style: TextStyle(
                       color: Color(0xFF3E3E3E),
                       fontFamily: 'Roboto',
-                      fontWeight: FontWeight.w700,  //semi-bold가 없으므로, bold으로 대체
+                      fontWeight: FontWeight.w700, //semi-bold가 없으므로, bold으로 대체
                       fontSize: 14,
                     ),
                   ),
