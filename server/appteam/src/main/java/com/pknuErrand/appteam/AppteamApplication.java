9d37d644a8afa1d6e4fe821614a8ddf93c11098c
unh6unh6 <ruddbs3308@naver.com>
2024-04-26 17:26:29 +0900

    Test: Release Test

--- server/appteam/src/main/java/com/pknuErrand/appteam/AppteamApplication.java
+++ server/appteam/src/main/java/com/pknuErrand/appteam/AppteamApplication.java
@@ -1,14 +1,18 @@
 package com.pknuErrand.appteam;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AppteamApplication {
 
+
 	public static void main(String[] args) {
 
 		SpringApplication.run(AppteamApplication.class, args);
+
+		System.out.println("develop - BE Branch");
 
 	}
 
