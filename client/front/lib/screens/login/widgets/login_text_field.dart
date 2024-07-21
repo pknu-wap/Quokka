aa2a0aeff0492d8e48da8100bfefbe1d801e1f72
김수현 <suhyun_113@naver.com>
2024-07-21 09:38:01 +0900

    Refactor: loginTextField 함수 구현

--- client/front/lib/screens/login/widgets/login_text_field.dart
+++ client/front/lib/screens/login/widgets/login_text_field.dart
@@ -9,3 +9,27 @@ BoxDecoration loginBoxDecoration()
     color: Color(0xFFE5E5E5),
   );
 }
+
+TextField loginTextField(int loginMaxLength, TextEditingController loginController){
+  return TextField(
+    maxLength: loginMaxLength,
+    // 최대 길이 설정
+    controller: loginController,
+    style: TextStyle(
+        color: Color(0xFF404040),
+        fontFamily: 'Pretendard',
+        fontWeight: FontWeight.w400),
+    decoration: InputDecoration(
+      hintText: '학번',
+      hintStyle: TextStyle(
+        fontFamily: 'Pretendard',
+        fontWeight: FontWeight.w400,
+        fontSize: 15.sp,
+        color: Color(0xFF404040),
+      ),
+      border: InputBorder.none,
+      counterText: '',
+    ),
+    keyboardType: TextInputType.number,
+  );
+}
