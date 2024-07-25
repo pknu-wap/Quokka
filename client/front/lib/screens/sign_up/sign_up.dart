feaeef435742a8323693c0680d6ac3ef84cab808
김수현 <suhyun_113@naver.com>
2024-07-26 08:10:53 +0900

    Refactor: 회원가입 페이지에 커스텀 앱 바 뒤로 가기 버튼 있는 버전 적용

--- client/front/lib/screens/sign_up/sign_up.dart
+++ client/front/lib/screens/sign_up/sign_up.dart
@@ -2,7 +2,7 @@ import 'dart:async'; //Timer이용 위함.
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:front/utils/button_utill.dart';
-import 'package:front/widgets/bar/custom_app_bar.dart';
+import 'package:front/widgets/bar/app_bar/custom_app_bar_back_button_yes.dart';
 import 'package:front/widgets/button/brown_button.dart';
 import 'package:front/widgets/text/button_text.dart';
 import '../../widgets/dialog/custom_dialog.dart';
@@ -230,7 +230,7 @@ class _SignUpScreenState extends State<SignUpScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CustomAppBar(
+      appBar: CustomAppBarBackButtonYes(
         title: '회원가입',
         onBackPressed: () {
           Navigator.of(context).pop();
