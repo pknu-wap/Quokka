b0d9f1c6776804c3becd31458b9d9babd5af28db
김수현 <suhyun_113@naver.com>
2024-07-27 01:01:06 +0900

    Feat: 마이페이지 점수별 색깔 측정 함수에 따른 배지 박스 바탕색 변동 함수 구현

--- client/front/lib/screens/main/my_page/my_page/utils/get_medal_box_util.dart
+++ client/front/lib/screens/main/my_page/my_page/utils/get_medal_box_util.dart
@@ -1,6 +1,8 @@
 // get_medal_box
 
 // 배지 박스 가로 길이
+import 'dart:ui';
+
 import 'change_color_util.dart';
 
 double getMedalBoxWidth(int score) {
@@ -18,5 +20,24 @@ double getMedalBoxWidth(int score) {
       return 28.0;
     default:
       return 21.0; // 임의로 저장
+  }
+}
+
+// 배지 박스 fillColor
+Color getMedalBoxFillColor(int score){
+  String color = changeColor(score);
+  switch (color) {
+    case "Bronze":
+      return Color(0xFFF9B887); // Bronze color
+    case "Silver":
+      return Color(0xFFDDDFE4); // Silver color
+    case "Gold":
+      return Color(0xFFFBE4AE); // Gold color
+    case "Purple":
+      return Color(0xFFB283EC); // Purple color
+    case "Black":
+      return Color(0xFF111211); // Black color
+    default:
+      return Color(0xFFD3D3D3); // Default color
   }
 }
