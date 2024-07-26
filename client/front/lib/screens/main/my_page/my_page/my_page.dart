b758af8479210f8821c1cebd123099ca937a095a
김수현 <suhyun_113@naver.com>
2024-07-27 04:20:00 +0900

    Refactor: 마이페이지 회원 부분 파일 적용

--- client/front/lib/screens/main/my_page/my_page/my_page.dart
+++ client/front/lib/screens/main/my_page/my_page/my_page.dart
@@ -1,15 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:front/screens/main/my_page/my_page/utils/get_medal_image_util.dart';
-import 'package:front/screens/main/my_page/widgets/my_page_text.dart';
+import 'package:front/screens/main/my_page/my_page/my_page_user.dart';
 import 'package:front/widgets/bar/app_bar/custom_app_bar_center.dart';
-
-import 'utils/change_color_util.dart';
-import 'utils/get_medal_box_util.dart';
-import 'widgets/my_page_medal_box.dart';
-import 'widgets/my_page_user_info_revise_button.dart';
-import 'widgets/my_page_user_ranked_button.dart';
 
 class MyPage extends StatefulWidget {
   @override
@@ -17,7 +8,6 @@ class MyPage extends StatefulWidget {
 }
 
 class _MyPageState extends State<MyPage> {
-  int _score = 600; // 평점
 
   @override
   Widget build(BuildContext context) {
@@ -31,76 +21,11 @@ class _MyPageState extends State<MyPage> {
       body: Center(
         child: Column(
           children: [
-            // 사용자 프로필
-            Container(
-              child: Stack(
-                children: [
-                  // 프로필 이미지
-                  Align(
-                    alignment: Alignment.centerLeft, // 왼쪽 정렬
-                    child: Container(
-                      margin: EdgeInsets.only(top: 43.h, left: 24.w),
-                      child: InkWell(
-                        onTap: () {
-                          print('Image button pressed');
-                        },
-                        child: SvgPicture.asset(
-                          'assets/images/Avatar.svg',
-                          width: 60.w,
-                          height: 56.h,
-                        ),
-                      ),
-                    ),
-                  ),
-                  // 사용자 닉네임
-                  Align(
-                    alignment: Alignment.centerLeft, // 왼쪽 정렬
-                    child: Container(
-                      margin: EdgeInsets.only(top: 54.h, left: 110.w),
-                      child: title('수현수현이'),
-                    ),
-                  ),
-                  // 회원 등급 페이지 이동 버튼
-                  UserRankedButton(
-                    score: _score,
-                    onTap: (){
-                      // Navigator.push(
-                      //   context,
-                      //   MaterialPageRoute(builder: (context) => UserRanked()));
-                    },
-                  ),
-                  // 회원 정보 수정 텍스트
-                  Align(
-                    alignment: Alignment.centerRight, // 왼쪽 정렬
-                    child: Container(
-                      margin: EdgeInsets.only(top: 48.h, right: 26.w),
-                      child: greyText("회원 정보 수정", 8.sp),
-                    ),
-                  ),
-                  // 회원 정보 수정 버튼
-                  Align(
-                    alignment: Alignment.centerRight,
-                    child: Container(
-                      margin: EdgeInsets.only(top: 54.h, right: 24.w),
-                      child: ElevatedButton(
-                        onPressed: () {
-                          print('회원 정보 수정 버튼 클릭');
-                          // Navigator.push(
-                          //   context,
-                          //   MaterialPageRoute(builder: (context) => UserInfo()));
-                        },
-                        style: userInfoReviseButton(),
-                        child: SvgPicture.asset(
-                          "assets/images/user_info_revise.svg",
-                          width: 20.w,
-                          height: 20.h,
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
+            // 사용자
+            MyPageUser(),
+            // 버튼 목록
+            // 설정 목록
+            // 로그아웃, 회원탈퇴 버튼
           ],
         ),
       ),
