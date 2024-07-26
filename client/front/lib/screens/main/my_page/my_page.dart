828eac9c3a04af4fbc23122fbb8e0d8aff7be820
김수현 <suhyun_113@naver.com>
2024-07-26 09:06:37 +0900

    Feat: 마이페이지 사용자 프로필 중 프로필 이미지 추가

--- client/front/lib/screens/main/my_page/my_page.dart
+++ client/front/lib/screens/main/my_page/my_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:front/widgets/bar/app_bar/custom_app_bar_center.dart';
 
 class MyPage extends StatefulWidget {
@@ -15,6 +17,38 @@ class _MyPageState extends State<MyPage> {
         onBackPressed: (){
           Navigator.of(context).pop();
         },
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            // 사용자 프로필
+            Container(
+              child: Stack(
+                children: [
+                  // 프로필 이미지
+                  Align(
+                    alignment: Alignment.centerLeft, // 왼쪽 정렬
+                    child: Container(
+                      margin: EdgeInsets.only(top: 43.h, left: 50),
+                      child: InkWell(
+                        onTap: () {
+                          print('Image button pressed');
+                        },
+                        child: SvgPicture.asset(
+                          'assets/images/Avatar.svg',
+                          width: 60.w,
+                          height: 56.h,
+                        ),
+
+                      ),
+                    ),
+                  ),
+
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
