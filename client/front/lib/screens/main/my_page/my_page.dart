462984cb24061780c54568b0e39b557475be89b6
김수현 <suhyun_113@naver.com>
2024-07-26 09:22:24 +0900

    Feat: 프로필 이미지 마진 수정 및 사용자 닉네임 추가

--- client/front/lib/screens/main/my_page/my_page.dart
+++ client/front/lib/screens/main/my_page/my_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:front/screens/main/my_page/widgets/my_page_text.dart';
 import 'package:front/widgets/bar/app_bar/custom_app_bar_center.dart';
 
 class MyPage extends StatefulWidget {
@@ -29,7 +30,7 @@ class _MyPageState extends State<MyPage> {
                   Align(
                     alignment: Alignment.centerLeft, // 왼쪽 정렬
                     child: Container(
-                      margin: EdgeInsets.only(top: 43.h, left: 50),
+                      margin: EdgeInsets.only(top: 43.h, left: 24),
                       child: InkWell(
                         onTap: () {
                           print('Image button pressed');
@@ -39,10 +40,18 @@ class _MyPageState extends State<MyPage> {
                           width: 60.w,
                           height: 56.h,
                         ),
-
                       ),
                     ),
                   ),
+                  // 사용자 닉네임
+                  Align(
+                    alignment: Alignment.centerLeft, // 왼쪽 정렬
+                    child: Container(
+                      margin: EdgeInsets.only(top: 54.h, left: 110.w),
+                      child: title('수현수현이'),
+                    ),
+                  ),
+
 
                 ],
               ),
