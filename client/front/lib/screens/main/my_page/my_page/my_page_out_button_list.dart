e55662ad8637f390d323f8f1a386aeca3d906c80
김수현 <suhyun_113@naver.com>
2024-07-29 04:01:29 +0900

    Feat: 마이페이지 가로 버튼 목록 중 텍스트 버튼 위젯 적용

---
+++ client/front/lib/screens/main/my_page/my_page/my_page_out_button_list.dart
@@ -0,0 +1,49 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'widgets/my_page_out_button.dart';
+import 'widgets/my_page_row_button_line.dart';
+
+class OutButtonList extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 10.h),
+      // 상자 만들기
+      width: 130.w,
+      height: 20.h,
+      child: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            // 로그아웃
+            OutButton(
+              buttonText: "로그아웃",
+              onTap: (){
+                print("로그아웃 버튼 클릭!");
+                // Navigator.push(
+                //   context,
+                //   MaterialPageRoute(builder: (context) => RecentErrand()));
+              },
+            ),
+            RowButtonLine(rowLineHeight: 10.h),
+            // 회원탈퇴
+            OutButton(
+              buttonText: "회원탈퇴",
+              onTap: (){
+                print("회원탈퇴 버튼 클릭!");
+                // Navigator.push(
+                //   context,
+                //   MaterialPageRoute(builder: (context) => RecentErrand()));
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+
+    // 안에, 가로로 정렬(버튼 세 개 만들기)
+    // 각각을 세로로 정렬(텍스트, 이미지)
+  }
+}
