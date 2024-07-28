2138dc6dea18a1b5964d39b7c3ed6b72d433aba4
김수현 <suhyun_113@naver.com>
2024-07-29 04:00:04 +0900

    Feat: 마이페이지 가로 버튼 구분선 height 매개변수로 받기 변경

--- client/front/lib/screens/main/my_page/my_page/widgets/my_page_row_button_line.dart
+++ client/front/lib/screens/main/my_page/my_page/widgets/my_page_row_button_line.dart
@@ -2,16 +2,18 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class RowButtonLine extends StatelessWidget {
+  final double rowLineHeight;
 
   const RowButtonLine({
     Key? key,
+    required this.rowLineHeight,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: 0.5.w,
-      height: 39.h,
+      height: rowLineHeight.h,
       decoration: BoxDecoration(
         color: Color(0xffA9A9A9),
       ),
