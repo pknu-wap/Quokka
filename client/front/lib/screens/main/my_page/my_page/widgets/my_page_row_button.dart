8c2a02fa42c9f893c30b2a5f655e2603d010c113
김수현 <suhyun_113@naver.com>
2024-07-28 19:48:55 +0900

    Feat: 마이페이지 가로 버튼 목록 중 각 버튼 위젯에서 텍스트 부분 구현

---
+++ client/front/lib/screens/main/my_page/my_page/widgets/my_page_row_button.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class RowButton extends StatelessWidget {
+  final String buttonText;
+  final String buttonIcon;
+  final VoidCallback onTap;
+
+  const RowButton({
+    Key? key,
+    required this.buttonText,
+    required this.buttonIcon,
+    required this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: 90.w,
+        height: 79.h,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              child: Text(
+                buttonText,
+                style: TextStyle(
+                  color: Color(0xff7E7E7E),
+                  fontSize: 10.sp,
+                  fontWeight: FontWeight.w400, // Regular
+                  fontFamily: "Pretendard",
+                  letterSpacing: 0.001,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
