ddc3a43ec547d2d28105de5dde2f9084bfcf7b7e
김수현 <suhyun_113@naver.com>
2024-08-03 04:15:05 +0900

    Feat: 프로필 이미지 사용자 선택 후 저장 시, 회원 정보 페이지의 프로필 이미지에 반영되도록 구현

--- client/front/lib/screens/main/my_page/user_info/user_info_user_image.dart
+++ client/front/lib/screens/main/my_page/user_info/user_info_user_image.dart
@@ -1,15 +1,21 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_svg/svg.dart';
 
 class UserImage extends StatelessWidget {
+  final String imagePath;
+  final VoidCallback onTap;
+
+  const UserImage({
+    Key? key,
+    required this.imagePath,
+    required this.onTap,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () {
-        print("프로필 이미지 변경 클릭!");
-      },
+      onTap: onTap,
       child: Container(
         margin: EdgeInsets.only(top: 90.h),
         width: 115.w,
@@ -19,22 +25,18 @@ class UserImage extends StatelessWidget {
         ),
         child: Stack(
           children: [
-            // 프로필 이미지
             Align(
               alignment: Alignment.center,
-              child: Container(
-                child: SvgPicture.asset(
-                  'assets/images/Avatar.svg',
-                  width: 95.w,
-                  height: 90.h,
-                ),
+              child: SvgPicture.asset(
+                imagePath,
+                width: 95.w,
+                height: 90.h,
               ),
             ),
-            // 카메라 이미지 버튼
             Align(
               alignment: Alignment.centerRight,
               child: Container(
-                margin: EdgeInsets.only(top: 55.h, right: 10.w), // Adjust margins
+                margin: EdgeInsets.only(top: 55.h, right: 10.w),
                 child: SvgPicture.asset(
                   'assets/images/camera.svg',
                   width: 21.w,
