f25dac155da92bfe36902cf654ea6c4f0a18b6e2
김수현 <suhyun_113@naver.com>
2024-08-03 04:16:30 +0900

    Feat: 프로필 이미지와 사용자 이름 정보 프로필 이미지 변경 슬라이드로 넘겨주기

--- client/front/lib/screens/main/my_page/user_info/my_page_user_info.dart
+++ client/front/lib/screens/main/my_page/user_info/my_page_user_info.dart
@@ -1,7 +1,6 @@
-// 회원 정보 수정
 import 'package:flutter/material.dart';
-
 import 'user_info_column_list.dart';
+import 'user_info_slide_up_bottom.dart';
 import 'user_info_user_image.dart';
 import 'widgets/my_page_app_bar.dart';
 
@@ -11,6 +10,19 @@ class MyPageUserInfo extends StatefulWidget {
 }
 
 class _MyPageUserInfoState extends State<MyPageUserInfo> {
+  String _profileImagePath = 'assets/images/Avatar.svg'; // Default image path
+
+  void _updateProfileImage(String newImagePath) {
+    setState(() {
+      _profileImagePath = newImagePath;
+    });
+  }
+
+  void _resetProfileImage() {
+    setState(() {
+      _profileImagePath = 'assets/images/Avatar.svg'; // Reset to default image
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,7 +31,28 @@ class _MyPageUserInfoState extends State<MyPageUserInfo> {
       body: Center(
         child: Column(
           children: [
-            UserImage(),
+            UserImage(
+              imagePath: _profileImagePath, // Pass current profile image
+              onTap: () {
+                showModalBottomSheet(
+                  context: context,
+                  isScrollControlled: true,
+                  backgroundColor: Colors.transparent,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+                  ),
+                  builder: (context) => SlideUpBottom(
+                    nickName: "수현수현이",
+                    initialImagePath: _profileImagePath,
+                    onImageSaved: (newImagePath) {
+                      _updateProfileImage(newImagePath);
+                    },
+                    onReset: _resetProfileImage,
+                  ),
+                );
+
+              },
+            ),
             ColumnList(),
           ],
         ),
