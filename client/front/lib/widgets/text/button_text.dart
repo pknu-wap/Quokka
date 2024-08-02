95e4c08f0fe67cb2fbe12ec7531e8d5c8bbeffb5
김수현 <suhyun_113@naver.com>
2024-08-03 04:07:48 +0900

    Feat: whiteButtonText 추가 정의

--- client/front/lib/widgets/text/button_text.dart
+++ client/front/lib/widgets/text/button_text.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+// 갈색 버튼 흰색 텍스트
 Text buttonText(String text)
 {
   return Text(
@@ -10,6 +11,21 @@ Text buttonText(String text)
       fontFamily: 'Pretendard',
       fontWeight: FontWeight.w600,
       color: Color(0xFFFFFFFF),
+      letterSpacing: 0.01,
+    ),
+  );
+}
+
+// 흰색 버튼 검정 텍스트
+Text whiteButtonText(String text)
+{
+  return Text(
+    text,
+    style: TextStyle(
+      fontSize: 15.sp,
+      fontFamily: 'Pretendard',
+      fontWeight: FontWeight.w600, //Semi-bold
+      color: Color(0xFF3E3E3E),
       letterSpacing: 0.01,
     ),
   );
