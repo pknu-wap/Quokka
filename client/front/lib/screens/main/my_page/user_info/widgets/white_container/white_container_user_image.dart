79f1cbba865c51d8db2448e3154b377fd4a69aae
김수현 <suhyun_113@naver.com>
2024-08-03 20:50:03 +0900

    Feat: 흰색 컨테이너 속 이미지 미리보기에 선택한 이미지 반영하도록 구현

--- client/front/lib/screens/main/my_page/user_info/widgets/white_container/white_container_user_image.dart
+++ client/front/lib/screens/main/my_page/user_info/widgets/white_container/white_container_user_image.dart
@@ -1,21 +1,33 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-
 class WhiteContainerUserImage extends StatelessWidget {
-  final String selectedImage;
+  final String selectedImagePath;
+  final bool isSvg;
 
-  WhiteContainerUserImage({required this.selectedImage});
+  const WhiteContainerUserImage({
+    Key? key,
+    required this.selectedImagePath,
+    this.isSvg = true,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.only(top: 35.39.h),
-      child: SvgPicture.asset(
-        selectedImage,
+      child: isSvg
+          ? SvgPicture.asset( // 커카 이미지
+        selectedImagePath,
         width: 86.w,
         height: 83.99.h,
+      )
+          : Image.file( // 카메라, 갤러리에서 선택한 이미지
+        File(selectedImagePath),
+        width: 86.w,
+        height: 83.99.h,
+        fit: BoxFit.cover,
       ),
     );
   }
