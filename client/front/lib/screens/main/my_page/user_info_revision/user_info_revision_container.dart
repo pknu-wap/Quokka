893a9a0aa6485207a5fdba8e77e6fe1d461126a2
김수현 <suhyun_113@naver.com>
2024-08-04 04:28:31 +0900

    Feat: 회원 정보 수정 페이지 각 요소인 컨테이너 구현(텍스트 필드 이름, 텍스트 필드 정보, 텍스트 필드 회색 설명 문자) 및 텍스트 위젯 적용

---
+++ client/front/lib/screens/main/my_page/user_info_revision/user_info_revision_container.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:front/screens/main/my_page/user_info_revision/widgets/user_info_revision_text.dart';
+import 'package:front/screens/main/my_page/widgets/my_page_text.dart';
+
+import 'widgets/user_info_revision_text_field.dart';
+
+class RevisionContainer extends StatelessWidget {
+  final TextEditingController controller;
+  final String labelText;
+  final TextInputType keyboardType;
+  final bool obscureText; // 텍스트 가리기
+  final String grayText;
+
+  const RevisionContainer({
+    Key? key,
+    required this.controller,
+    required this.labelText,
+    required this.keyboardType,
+    this.obscureText = false,
+    this.grayText = "",
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        // 텍스트 필드 이름
+        title(labelText),
+        // 텍스트 필드(정보)
+        RevisionTextField(
+          controller: controller,
+          text: "",
+          keyboardType: keyboardType,
+          obscureText: obscureText,
+        ),
+        // 텍스트 필드 회색 설명 문자
+        revisionGrayText(grayText),
+      ],
+    );
+  }
+}
